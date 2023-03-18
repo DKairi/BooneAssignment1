ab05607be85d34c8e99ef502164633a0e593f716
D'Kairi Boone <d.boone@setonhill.edu>
2023-03-18 19:16:09 -0400

    Initial Commit

---
+++ BooneAssignment1/BooneAssignment1App.swift
@@ -0,0 +1,17 @@
+//
+//  BooneAssignment1App.swift
+//  BooneAssignment1
+//
+//  Created by d.boone on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct BooneAssignment1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
