b909d6f18bbc2fd606515fa8aabe064d69090514
D'Kairi Boone <d.boone@setonhill.edu>
2023-03-18 20:52:18 -0400

    Ready

--- BooneAssignment1/ContentView.swift
+++ BooneAssignment1/ContentView.swift
@@ -1,4 +1,4 @@
-//
+
 //  ContentView.swift
 //  BooneAssignment1
 //
@@ -10,13 +10,18 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            
+            Text("Sports Stats!")
+                .foregroundColor(Color.red)
+                .multilineTextAlignment(.center)
         }
+        
         .padding()
-    }
+        
+        .frame(width: 100.0)
+              }
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
